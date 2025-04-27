41deea64a0828d84b37566e7a18a9b82ad52356f
Benjamin Smidt <benjamin.raymond.smidt@gmail.com>
2025-04-27 13:44:34 -0700

    test(agent): add tests for checking if agent is activated

---
+++ agent/tests/storage/agent.rs
@@ -0,0 +1,43 @@
+// internal crates
+use config_agent::filesys::dir::Dir;
+use config_agent::storage::{
+    agent::{Agent, assert_activated},
+    errors::StorageErr,
+};
+
+pub mod assert_activated {
+    use super::*;
+
+    #[tokio::test]
+    async fn file_does_not_exist() {
+        let dir = Dir::create_temp_dir("testing").await.unwrap();
+        let agent_file = dir.file("agent.json");
+
+        let result = assert_activated(&agent_file).await.unwrap_err();
+        assert!(matches!(result, StorageErr::FileSysErr { .. }));
+    }
+
+    #[tokio::test]
+    async fn invalid_file_contents() {
+        let dir = Dir::create_temp_dir("testing").await.unwrap();
+        let agent_file = dir.file("agent.json");
+        agent_file.write_string("not a valid agent", true, true).await.unwrap();
+
+        let result = assert_activated(&agent_file).await.unwrap_err();
+        assert!(matches!(result, StorageErr::FileSysErr { .. }));
+    }
+
+    #[tokio::test]
+    async fn agent_not_activated() {
+        let dir = Dir::create_temp_dir("testing").await.unwrap();
+        let agent_file = dir.file("agent.json");
+        let agent = Agent {
+            activated: false,
+            ..Default::default()
+        };
+        agent_file.write_json(&agent, true, true).await.unwrap();
+
+        let result = assert_activated(&agent_file).await.unwrap_err();
+        assert!(matches!(result, StorageErr::AgentNotActivatedErr { .. }));
+    }
+}
--- agent/tests/storage/mod.rs
+++ agent/tests/storage/mod.rs
@@ -1,2 +1,3 @@
+pub mod agent;
 pub mod cache;
 pub mod setup;
