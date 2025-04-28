a1cb1300bde9819d173f05d489bb090ff2a47a14
Benjamin Smidt <benjamin.raymond.smidt@gmail.com>
2025-04-28 16:38:14 -0700

    feat: add direct install script for install / activate in single cmd

---
+++ direct-install.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+set -e
+
+# install the debian package
+echo "Installing the Miru Agent"
+echo "========================="
+curl -fsSL https://raw.githubusercontent.com/miruml/agent/main/direct-deb-install.sh | sh
+
+# install the agent
+echo ""
+echo ""
+echo "Activating the Miru Agent"
+echo "========================="
+curl -fsSL https://raw.githubusercontent.com/miruml/agent/main/activate.sh | sh
