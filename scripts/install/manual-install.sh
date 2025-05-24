3af5f2b2dc7e56689ffd253b05c853a7f36dd7d4
Benjamin Smidt <ben@miruml.com>
2025-05-24 15:46:47 -0700

    fix: fix install script to take git branch arg and fix script paths

--- scripts/install/manual-install.sh
+++ scripts/install/manual-install.sh
@@ -1,10 +1,13 @@
 #!/bin/sh
 set -e
 
+# get the branch name
+branch=${1:-"main"}
+
 # install the debian package
 echo "Installing the Miru Agent"
 echo "========================="
-curl -fsSL https://raw.githubusercontent.com/miruml/agent/main/manual-deb-install.sh | sh
+curl -fsSL https://raw.githubusercontent.com/miruml/agent/"$branch"/scripts/install/manual-deb-install.sh | sh
 
 # install the agent
 echo ""
@@ -13,9 +16,9 @@ echo "Activating the Miru Agent"
 echo "========================="
 BACKEND_URL=${1:-""}
 if [ -n "$BACKEND_URL" ]; then
-    curl -fsSL https://raw.githubusercontent.com/miruml/agent/main/activate.sh | sh -s -- "$BACKEND_URL"
+    curl -fsSL https://raw.githubusercontent.com/miruml/agent/"$branch"/scripts/install/activate.sh | sh -s -- "$BACKEND_URL"
 else
-    curl -fsSL https://raw.githubusercontent.com/miruml/agent/main/activate.sh | sh
+    curl -fsSL https://raw.githubusercontent.com/miruml/agent/"$branch"/scripts/install/activate.sh | sh
 fi
 
 exit 0
