50ac244cc634c6bbbae7de7dd97462e96a601668
Benjamin Smidt <ben@miruml.com>
2025-05-25 07:55:45 -0700

    ci: remove support for nightly builds

--- build/changelog.sh
+++ build/changelog.sh
@@ -5,8 +5,8 @@ this_repo_root_dir=$(git rev-parse --show-toplevel)
 this_dir=$this_repo_root_dir/build
 cd "$this_dir"
 
-# shellcheck source=previous_tag.sh
-. "$this_dir/previous_tag.sh"
+# shellcheck source=git-tags.sh
+. "$this_dir/git-tags.sh"
 previous_tag=$(previous_tag)
 echo "Previous tag: $previous_tag"
 export GORELEASER_PREVIOUS_TAG="$previous_tag"
