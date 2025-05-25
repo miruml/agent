39512c416fd235092b57364159a02545c60e54c3
Benjamin Smidt <ben@miruml.com>
2025-05-25 15:15:06 -0700

    ci: fix previous tag determination logic

--- build/git-tags.sh
+++ build/git-tags.sh
@@ -1,16 +1,33 @@
 #!/bin/sh
 set -e
 
+current_tag () {
+    git describe --exact-match --tags HEAD
+}
+
+current_tags () {
+    git tag --points-at HEAD
+}
+
 # determine the previous tag by tags which follow 'vX.y.z' but have no characters after
-# the 'z'
+# the 'z' AND are not the current tag
 previous_tag () {
-    git tag -l 'v[0-9]*.[0-9]*.[0-9]*' | grep -E '^v[0-9]+\.[0-9]+\.[0-9]+$' | sort -V | tail -n 1
+    git tag -l 'v[0-9]*.[0-9]*.[0-9]*' \
+        | grep -E '^v[0-9]+\.[0-9]+\.[0-9]+$' \
+        | grep -v -F -x "$(current_tags)" \
+        | sort -V \
+        | tail -n 1
 }
 
 latest_tag () {
     git tag -l 'v[0-9]*.[0-9]*.[0-9]*' | sort -V | tail -n 1
 }
 
-current_commit_tag () {
-    git describe --exact-match --tags HEAD
-}
+echo "Current tag: $(current_tag)"
+echo "Current tags:"
+for tag in $(current_tags); do
+    echo "  $tag"
+done
+echo "Previous tag: $(previous_tag)"
+echo "Latest tag: $(latest_tag)"
+
