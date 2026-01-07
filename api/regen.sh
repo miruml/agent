5a3265a85b64a1bbb1b9e6e4c7841734c7848313
Benjamin Smidt <ben@miruml.com>
2026-01-07 12:54:19 -0800

    refactor: restructure openapi code generation
    Remove openapi git submodule and move specs to local api/specs/ directory.
    Rename gen/ to codegen/ (gitignored) and update Makefile/regen.sh with
    clearer variable names (backend-client, agent-server).

--- api/regen.sh
+++ api/regen.sh
@@ -3,7 +3,7 @@ set -e
 
 git_repo_root_dir=$(git rev-parse --show-toplevel)
 this_dir=$git_repo_root_dir/api
-gen_dir=$this_dir/gen
+codegen_dir=$this_dir/codegen
 lint_script=$git_repo_root_dir/scripts/lint.sh
 
 # generate the models
@@ -11,31 +11,31 @@ cd "$this_dir"
 make gen
 
 # client
-gen_client_models_dir=$gen_dir/client/src/models
-client_models_target_dir=$git_repo_root_dir/libs/openapi-client/src/models
+codegen_backend_client_models_dir=$codegen_dir/backend-client/src/models
+backend_client_models_target_dir=$git_repo_root_dir/libs/openapi-client/src/models
 
 # replace the target model directories with the generated ones
-rm -rf "${client_models_target_dir:?}"/*
+rm -rf "${backend_client_models_target_dir:?}"/*
 
 # copy all the files in the generated models dirs to the target models dirs
-if [ ! -d "$client_models_target_dir" ]; then
-    mkdir "$client_models_target_dir"
+if [ ! -d "$backend_client_models_target_dir" ]; then
+    mkdir "$backend_client_models_target_dir"
 fi
-cp -r "$gen_client_models_dir"/* "$client_models_target_dir"
+cp -r "$codegen_backend_client_models_dir"/* "$backend_client_models_target_dir"
 
 
 # server
-gen_server_models_dir=$gen_dir/server/src/models
-server_models_target_dir=$git_repo_root_dir/libs/openapi-server/src/models
+codegen_agent_server_models_dir=$codegen_dir/agent-server/src/models
+agent_server_models_target_dir=$git_repo_root_dir/libs/openapi-server/src/models
 
 # replace the target model directories with the generated ones
-rm -rf "${server_models_target_dir:?}"/*
+rm -rf "${agent_server_models_target_dir:?}"/*
 
 # copy all the files in the generated models dirs to the target models dirs
-if [ ! -d "$server_models_target_dir" ]; then
-    mkdir "$server_models_target_dir"
+if [ ! -d "$agent_server_models_target_dir" ]; then
+    mkdir "$agent_server_models_target_dir"
 fi
-cp -r "$gen_server_models_dir"/* "$server_models_target_dir"
+cp -r "$codegen_agent_server_models_dir"/* "$agent_server_models_target_dir"
 
 # lint the repository so that generated code doesn't cause huge diffs
 $lint_script
